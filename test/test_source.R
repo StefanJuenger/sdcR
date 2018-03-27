552a1acdb9cd38ae5322c57ac9d52eb40998b218
mueller2 <stefan.mueller@gesis.org>
2018-03-27 17:06:44 +0200

    added test files

---
+++ test/test_source.R
@@ -0,0 +1,23 @@
+# PLEASE DEFINE PATH AND NAME OF THIS FILE IN THE FOLLOWING VARIABLE
+this_file <- "./test/test_source.R"
+
+
+# PLACE CODE HERE --------------------------------------------------------------
+
+
+# fancy comment
+x <- 1
+x
+y # produces an error
+
+# shit, doesn't work
+bla <- "bla"
+
+plot(1:100)
+
+
+# STOP ADDING CODE BELOW -------------------------------------------------------
+
+# DO NOT CHANGE OR EDIT THE FOLLOWING LINES. MUST REMAIN AT THE END OF THIS FILE
+produce_log_file(source_file = this_file,
+                 log_file = paste0(this_file, ".log"))

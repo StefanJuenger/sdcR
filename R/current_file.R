44a259503a2e0b05617a2c6ea3d2fb824e7b13f2
Müller <mueller2@gesis.org>
2019-02-27 16:00:44 +0100

    function that extracts current file name

---
+++ R/current_file.R
@@ -0,0 +1,45 @@
+#' https://stackoverflow.com/a/36777602
+#' current script file (in full path)
+#' @description current script file (in full path)
+#' @examples
+#' works with Rscript, source() or in RStudio Run selection, RStudio Console
+#' @export
+current_file <- function() {
+  # http://stackoverflow.com/a/32016824/2292993
+  cmdArgs = commandArgs(trailingOnly = FALSE)
+  needle = "--file="
+  match = grep(needle, cmdArgs)
+  if (length(match) > 0) {
+    # Rscript via command line
+    return(normalizePath(sub(needle, "", cmdArgs[match])))
+  } else {
+    ls_vars = ls(sys.frames()[[1]])
+    if ("fileName" %in% ls_vars) {
+      # Source'd via RStudio
+      return(normalizePath(sys.frames()[[1]]$fileName))
+    } else {
+      if (!is.null(sys.frames()[[1]]$ofile)) {
+        # Source'd via R console
+        return(normalizePath(sys.frames()[[1]]$ofile))
+      } else {
+        # RStudio Run Selection
+        # http://stackoverflow.com/a/35842176/2292993
+        pth = rstudioapi::getActiveDocumentContext()$path
+        if (pth!='') {
+          return(normalizePath(pth))
+        } else {
+          # RStudio Console
+          tryCatch({
+            pth = rstudioapi::getSourceEditorContext()$path
+            pth = normalizePath(pth)
+          }, error = function(e) {
+            # normalizePath('') issues warning/error
+            pth = ''
+          }
+          )
+          return(pth)
+        }
+      }
+    }
+  }
+}
